1e0b16ac1c2aec67c5a122a4b45a49a9ee86ab71
Tim Paine <3105306+timkpaine@users.noreply.github.com>
2026-07-26 21:42:43 -0500

    fix some lints

--- hatch_multi/structs.py
+++ hatch_multi/structs.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from typing import List, Optional, Union
-
 from pydantic import AliasChoices, BaseModel, Field
 
 __all__ = ("HatchMultiConfig",)
@@ -9,7 +7,7 @@ __all__ = ("HatchMultiConfig",)
 
 class HatchMultiConfig(BaseModel, validate_assignment=True):
     name: str = Field(description="Name of the package")
-    primary: Optional[Union[str, List[str]]] = Field(
+    primary: str | list[str] | None = Field(
         default=None,
         description="Optional dependency set/s to use as primary",
         alias=AliasChoices("primary", "default"),
